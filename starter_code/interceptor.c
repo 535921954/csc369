86d0652e6176a1bd3875e1322366eb4f146d5156
Edwin Chan <edwinkkc@gmail.com>
2016-06-08 10:28:32 -0400

    changed some formatting for consistency

--- starter_code/interceptor.c
+++ starter_code/interceptor.c
@@ -420,33 +420,36 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
      }
   }
 
-  else if(cmd == REQUEST_STOP_MONITORING){
-  //error handling
-  if (pid < 0 || (pid != 0 && pid_task(find_vpid(pid), PIDTYPE_PID) == NULL)) {
-          return -EINVAL;
-      }
-      if (current_uid() != 0) {
-          return -EPERM;
-      }
-     //check if the syscall is not intercepted
-        if (table[syscall].intercepted == 0) {
-            return -EINVAL;
-        }
-     //Check if not being monitored
-  if(check_pid_monitored(syscall, pid) == 0){
+  else if (cmd == REQUEST_STOP_MONITORING) {
+
+    //error handling
+    if (pid < 0 || (pid != 0 && pid_task(find_vpid(pid), PIDTYPE_PID) == NULL)) {
       return -EINVAL;
-  }
-  spin_lock(&pidlist_lock);
-  if(pid == 0){
-    //stop monitor for this syscall
-    destroy_list(syscall);
-  }
-  else{
-    //stop monitor the specified process for this syscall
+    }
+    if (current_uid() != 0) {
+      return -EPERM;
+    }
+    //check if the syscall is not intercepted
+    if (table[syscall].intercepted == 0) {
+      return -EINVAL;
+    }
+    //Check if not being monitored
+    if (check_pid_monitored(syscall, pid) == 0) {
+      return -EINVAL;
+    }
+
+    spin_lock(&pidlist_lock);
+    if (pid == 0) {
+    // stop monitor for this syscall
+      destroy_list(syscall);
+    }
+    else {
+    // stop monitor the specified process for this syscall
     del_pid_sysc(pid, syscall);
+    }
+    spin_unlock(&pidlist_lock);
   }
-  spin_unlock(&pidlist_lock);
-  }
+
   return 0;
 }
 
