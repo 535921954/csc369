29163ad6972a187b63640707c9e9ea63fa78cf4d
Edwin Chan <edwinkkc@gmail.com>
2016-06-08 01:53:25 -0400

    Added some comments, first attempt at REQUEST_START_MONITORING

--- starter_code/interceptor.c
+++ starter_code/interceptor.c
@@ -346,54 +346,84 @@ asmlinkage long interceptor(struct pt_regs reg) {
 asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 	/* Check if everything is valid first, then split into four parts
 	   depending on type of command */
-	
-	//Check if the syscall is valid. 
+
+	/* Check if the syscall is valid. */
 	if ((syscall < 0) || (syscall > NR_syscalls) || (syscall == MY_CUSTOM_SYSCALL)) {
 		return -EINVAL;
 	}
-	
+
 	if (cmd == REQUEST_SYSCALL_INTERCEPT) {
+    /* Error handling */
 		if (current_uid() != 0) {
-			return -EPERM; 
+			return -EPERM;
 		}
-		
 		if (table[syscall].intercepted == 1) {
-			return -EBUSY; 
+			return -EBUSY;
 		}
-			
-		table[syscall].intercepted = 1; 
+    /* Flag as intercepted and save original syscall */
+		table[syscall].intercepted = 1;
 		table[syscall].f = sys_call_table[syscall];
-		
+
+    /* Send syscall to interceptor */
 		spin_lock(&calltable_lock);
 		set_addr_rw((unsigned long)sys_call_table);
 		sys_call_table[syscall] = &interceptor;
 		set_addr_ro((unsigned long)sys_call_table);
 		spin_unlock(&calltable_lock);
-		
+
 	}
-	
+
 	else if (cmd == REQUEST_SYSCALL_RELEASE) {
+    /* Error handling */
 		if (current_uid() != 0) {
-			return -EPERM; 
+			return -EPERM;
 		}
-		
+
 		if (table[syscall].intercepted == 0) {
-			return -EINVAL; 
+			return -EINVAL;
 		}
-		
-		table[syscall].intercepted = 0; 
+
+    /* Flag as no longer intercepted and delete pid list */
+		table[syscall].intercepted = 0;
 		destroy_list(syscall);
-		
+
+    /* Restore hijacked syscall to original */
 		spin_lock(&calltable_lock);
 		set_addr_rw((unsigned long)sys_call_table);
 		sys_call_table[syscall] = table[syscall].f;
 		set_addr_ro((unsigned long)sys_call_table);
 		spin_unlock(&calltable_lock);
-		
+
 	}
-	
-	return 0; 
+
+  else if (cmd == REQUEST_START_MONITORING) {
+    /* Error handling */
+    if (pid < 0 || (pid != 0 && pid_task(find_vpid(pid), PIDTYPE_PID) == NULL) {
+      return -EINVAL;
+    }
+    if (current_uid() != 0) {
+      return -EPERM;
+    }
+
+    /* Check if already being monitored, otherwise add to list of
+     * monitored pids */
+     if (check_pid_monitored(syscall, pid) == 1) {
+       return -EBUSY;
+     }
+     else {
+       spin_lock(&pidlist_lock);
+       if (add_pid_sysc(pid, syscall) != 0) {
+         spin_unlock(&pidlist_lock);
+         return -ENOMEM;
+       }
+       spin_unlock(&pidlist_lock);
+     }
+
+  }
+
+	return 0;
 }
+
 
 	/*
 	if (cmd == REQUEST_START_MONITORING) {
@@ -405,7 +435,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
   } */
 /*
   return 0;
-  
+
 }
 */
 /*
