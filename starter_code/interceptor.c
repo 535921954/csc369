afdb5ef5624dfbd5f42519ddc3c46e032b4224eb
liujinbangbang <535921954@qq.com>
2016-06-08 18:35:33 -0400

    Update interceptor.c

--- starter_code/interceptor.c
+++ starter_code/interceptor.c
@@ -399,9 +399,11 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
   }
   
   else if (cmd == REQUEST_START_MONITORING) {
+  	//check if pid is valid
 	  if (pid < 0 || (pid != 0 && pid_task(find_vpid(pid), PIDTYPE_PID) == NULL)) {
 		  return -EINVAL; 
 	  }
+	  //check root
 	  if (current_uid() != 0) {
 		  if (pid == 0) {
 			  return -EPERM; 
@@ -410,11 +412,14 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			  return -EPERM; 
 		  }
 	  }
+	  //all this syscall's processes monitored
 	  if (pid == 0) {
 		  table[syscall].monitored = 2; 
 	  }
+	  //monitor the specified pid's process
 	  else {
 		  table[syscall].monitored = 1;
+		  //check if the process is already being monitored
 		  if (check_pid_monitored(syscall, pid) == 1) {
 			  return -EBUSY; 
 		  }
@@ -430,9 +435,11 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
   }
   
   else if (cmd == REQUEST_STOP_MONITORING) {
+  	//check if pid is valid
 	  if (pid < 0 || (pid != 0 && pid_task(find_vpid(pid), PIDTYPE_PID) == NULL)) {
 		  return -EINVAL; 
 	  }
+	  //check root
 	  if (current_uid() != 0) {
 		  if (pid == 0) {
 			  return -EPERM; 
@@ -441,17 +448,21 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			  return -EPERM; 
 		  }
 	  }
+	  //check if intercepted
 	  if (table[syscall].intercepted == 0) {
 		  return -EINVAL; 
 	  }
+	  //all this syscall's processes monitored,stop them
 	  if (pid == 0) {
 		  destroy_list(syscall); 
 		  table[syscall].monitored = 0; 
 	  }
 	  else {
+	  	//check if the pid is monitored
 		  if (check_pid_monitored(syscall, pid) == 0) {
 			  return -EINVAL; 
 		  }
+		  //the pid is monitored,stop the pid's process
 		  if (table[syscall].monitored == 1) {
 			  spin_lock(&pidlist_lock);
 			  if (del_pid_sysc(pid, syscall) != 0) {
