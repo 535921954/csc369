21e8d59e454baeb8c6cf4897aaaf0a9e0e2c0d17
Athavan Karunakaran <athavan_94@hotmail.com>
2016-08-02 21:50:19 -0400

    Update ext2_rm.c

--- A3/ext2_rm.c
+++ A3/ext2_rm.c
@@ -30,6 +30,22 @@ in this separate source file. */
 #include <sys/mman.h>
 #include "ext2.h"
 
+unsigned char *disk; 
+
 int main(int argc, char **argv) {
+  if argc != 3) {
+    fprintf(stderr, "Usage: reading <image file name> <file to be deleted>\n"); 
+    exit(1); 
+  }
+  int fd = open(argv[1], 0_RDWR); 
+  
+  disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
+  if (disk == MAP_FAILED) {
+    perror("mmap"); 
+    exit(1); 
+  }
+  
+  struct ext2_super_block *sb = (struct ext2_super_block *)(disk + 1024);
+  
   return 0;
 }
